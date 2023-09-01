c6896e308390af78474850372ca57a7baa1d8202
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-09-01 11:52:43 +0200

    data

---
+++ math.js
@@ -0,0 +1,25 @@
+const { sumPlus, sumMinus, sumMultiply,sumDivide } = require("./math");
+
+test("sum add two numbers", () => {
+    const result = sum(20, 30)
+    const expected = 50;
+    expected(result).toBe(expected);
+});
+
+test("sum add two numbers", () => {
+    const result = sum(20, 30)
+    const expected = 50;
+    expected(result).toBe(expected);
+});
+
+test("sum add two numbers", () => {
+    const result = sum(20, 30)
+    const expected = 50;
+    expected(result).toBe(expected);
+});
+
+test("sum add two numbers", () => {
+    const result = sum(20, 30)
+    const expected = 50;
+    expected(result).toBe(expected);
+});
